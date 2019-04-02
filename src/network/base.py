074f9fd13bbc1ac946f1323679d8ce896fd2c7d2
anhlt <tuananh.kirimaru@gmail.com>
2019-04-02 15:09:29 +0900

    base

--- src/network/base.py
+++ src/network/base.py
@@ -1,4 +1,6 @@
 from torch import nn
+from torch.nn import MaxPool2d
+import torch
 
 
 class Conv2d(nn.Module):
@@ -37,7 +39,198 @@ class Conv2d(nn.Module):
 class BottleneckBlock(nn.Module):
     """docstring for BottleneckBlock"""
 
-    def __init__(self, outer_filter: int, bottleneck_filter: int):
+    def __init__(self, input_channels: int, outer_filter: int, bottleneck_filter: int):
         super(BottleneckBlock, self).__init__()
+        self.input_channels = input_channels
         self.outer_filter = outer_filter
         self.bottleneck_filter = bottleneck_filter
+
+        self.first_layer = Conv2d(input_channels, outer_filter, 3, same_padding=True, bn=True)
+        self.second_layer = Conv2d(outer_filter, bottleneck_filter, 1, same_padding=True, bn=True)
+        self.third_layer = Conv2d(bottleneck_filter, outer_filter, 3, same_padding=True, bn=True)
+
+    def forward(self, x):
+        x = self.first_layer(x)
+        x = self.second_layer(x)
+        x = self.third_layer(x)
+        return x
+
+
+class DoubleBottleneckBlock(nn.Module):
+    """docstring for DoubleBottleneckBlock"""
+
+    def __init__(self, input_channels: int, outer_filter: int, bottleneck_filter: int):
+        super(DoubleBottleneckBlock, self).__init__()
+        self.input_channels = input_channels
+        self.outer_filter = outer_filter
+        self.bottleneck_filter = bottleneck_filter
+
+        self.first_layer = BottleneckBlock(input_channels, outer_filter, bottleneck_filter)
+        self.second_layer = Conv2d(outer_filter, bottleneck_filter, 1, same_padding=True, bn=True)
+        self.third_layer = Conv2d(bottleneck_filter, outer_filter, 3, same_padding=True, bn=True)
+
+    def forward(self, x):
+        x = self.first_layer(x)
+        x = self.second_layer(x)
+        x = self.third_layer(x)
+        return x
+
+
+class DarknetBodyBottom(nn.Module):
+    """docstring for DarknetBodyBottom"""
+
+    def __init__(self, **kwargs):
+        super(DarknetBodyBottom, self).__init__()
+        self.first_layer = Conv2d(3, 32, 3, **kwargs)
+        self.second_layer = MaxPool2d(2)
+        self.third_layer = Conv2d(32, 64, 3, **kwargs)
+        self.forth_layer = MaxPool2d(2)
+        self.fifth_layer = BottleneckBlock(64, 128, 64)
+        self.sixth_layer = MaxPool2d(2)
+        self.seventh_layer = BottleneckBlock(128, 256, 128)
+        self.eighth_layer = MaxPool2d(2)
+        self.nineth_layer = DoubleBottleneckBlock(256, 512, 256)
+
+    def forward(self, x):
+        x = self.first_layer(x)
+        print(x.shape)
+        x = self.second_layer(x)
+        print(x.shape)
+
+        x = self.third_layer(x)
+        x = self.forth_layer(x)
+        print(x.shape)
+        x = self.fifth_layer(x)
+        x = self.sixth_layer(x)
+        print(x.shape)
+        x = self.seventh_layer(x)
+        x = self.eighth_layer(x)
+        print(x.shape)
+        x = self.nineth_layer(x)
+        print(x.shape)
+        return x
+
+
+class DarknetBodyHead(nn.Module):
+    """docstring for DarknetBodyHead"""
+
+    def __init__(self):
+        super(DarknetBodyHead, self).__init__()
+
+        self.tenth_layer = MaxPool2d(2)
+        self.eleventh_layer = DoubleBottleneckBlock(512, 1024, 512)
+
+    def forward(self, x):
+
+        x = self.tenth_layer(x)
+        x = self.eleventh_layer(x)
+        return x
+
+
+class DarknetBody(nn.Module):
+    """docstring for DarknetBody"""
+
+    def __init__(self):
+        super(DarknetBody, self).__init__()
+        kwargs = {
+            "same_padding": True,
+            "bn": True
+        }
+
+        self.body_bottom = DarknetBodyBottom(**kwargs)
+        self.body_head = DarknetBodyHead()
+
+    def forward(self, x):
+
+        x = self.body_bottom(x)
+        x = self.body_head(x)
+
+        return x
+
+
+class DarkNet(nn.Module):
+    """docstring for DarkNet"""
+
+    def __init__(self):
+        super(DarkNet, self).__init__()
+        self.body = DarknetBody()
+        self.head = nn.Conv2d(1024, 1000, 1)
+        self.global_avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.active = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = self.body(x)
+        x = self.head(x)
+        x = self.global_avg_pool(x)
+        x = self.active(x)
+        return x
+
+
+class Reorg(nn.Module):
+    def __init__(self, stride=2):
+        super(Reorg, self).__init__()
+        self.stride = stride
+
+    def forward(self, x):
+        stride = self.stride
+        assert(x.data.dim() == 4)
+        B = x.data.size(0)
+        C = x.data.size(1)
+        H = x.data.size(2)
+        W = x.data.size(3)
+        assert(H % stride == 0)
+        assert(W % stride == 0)
+        ws = stride
+        hs = stride
+        x = x.view(B, C, H // hs, hs, W // ws, ws).transpose(3, 4).contiguous()
+        x = x.view(B, C, H // hs * W // ws, hs * ws).transpose(2, 3).contiguous()
+        x = x.view(B, C, hs * ws, H // hs, W // ws).transpose(1, 2).contiguous()
+        x = x.view(B, hs * ws * C, H // hs, W // ws)
+        return x
+
+
+class YoloBody(nn.Module):
+    """docstring for YoloBody"""
+
+    def __init__(self, num_anchors, num_classes):
+        super(YoloBody, self).__init__()
+        kwargs = {
+            "same_padding": True,
+            "bn": True
+        }
+        self.num_classes = num_classes
+        self.num_anchors = num_anchors
+
+        self.body_bottom = DarknetBodyBottom(**kwargs)
+        self.body_head = DarknetBodyHead()
+
+        self.first_layer = Conv2d(1024, 1024, 3, same_padding=True, bn=True)
+        self.second_layer = Conv2d(1024, 1024, 3, same_padding=True, bn=True)
+
+        self.find_grain = Conv2d(512, 64, 1, same_padding=True, bn=True)
+        self.re_org = Reorg(stride=2)
+
+        self.after_concat = Conv2d(256 + 1024, 1024, 3, same_padding=True, bn=True)
+
+        self.last_layer = nn.Conv2d(1024, num_anchors * (5 + num_classes), 1, 1, 0, bias=False)
+
+    def forward(self, x):
+        print(x.shape)
+        x1 = self.body_bottom(x)
+        print(x1.shape)
+        x = self.body_head(x1)
+        print(x.shape)
+        x = self.first_layer(x)
+        x = self.second_layer(x)
+
+        print(x.shape)
+
+        x1 = self.find_grain(x1)
+        print(x1.shape)
+        x1 = self.re_org(x1)
+        print(x1.shape)
+
+        x = torch.cat((x, x1), dim=1)
+        x = self.after_concat(x)
+        x = self.last_layer(x)
+        return x
