9c656527b87ab425491373457b48a5ff7dcc77a3
anhlt <tuananh.kirimaru@gmail.com>
2019-04-07 16:06:35 +0900

    fix process boxes error

--- src/utils/process_boxes.py
+++ src/utils/process_boxes.py
@@ -48,8 +48,9 @@ def preprocess_true_boxes(true_boxes, anchors, image_size, num_classes):
 
         box = box[0:4] * np.array(
             [conv_width, conv_height, conv_width, conv_height])
-        i = np.floor(box[1]).astype('int')
-        j = min(np.floor(box[0]).astype('int'), 1)
+        i = min(height - 1,max(np.floor(box[1]).astype('int'), 0))
+        j = min(width - 1 ,max(np.floor(box[0]).astype('int'), 0))
+        
         best_iou = 0
         best_anchor = 0
 
@@ -75,7 +76,8 @@ def preprocess_true_boxes(true_boxes, anchors, image_size, num_classes):
             detectors_mask[best_anchor, :, i, j] = 1
             adjusted_box = np.array(
                 [
-                    box[0] - j, box[1] - i,
+                    box[0] - j,
+                    box[1] - i,
                     np.log(box[2] / anchors[best_anchor][0]),
                     np.log(box[3] / anchors[best_anchor][1])
                 ],
